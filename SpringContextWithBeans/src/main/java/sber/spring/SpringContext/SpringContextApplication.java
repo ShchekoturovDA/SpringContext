235f005eb490e5e21a90f59e1bb2a17a94e4d363
ShchekoturovDA <shchekoturovdmitr@mail.ru>
2024-07-02 20:54:41 +0300

    tsk2 and tsk3

---
+++ SpringContextWithBeans/src/main/java/sber/spring/SpringContext/SpringContextApplication.java
@@ -0,0 +1,14 @@
+package sber.spring.SpringContext;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContextApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(SpringContextApplication.class, args);
+	}
+
+}
